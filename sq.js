5a086459a65f0cd37d685a3d7829c49b894ca49d
Greg Ros <life.gregr@gmail.com>
2022-10-13 15:20:25 +0300

    save

--- sq.js
+++ sq.js
@@ -1,1 +1,24 @@
-It's not empty now.
+// Logs user in using `auth`
+export async function login(auth) {
+    // ... GET REQUEST ...
+}
+
+// Gets a list of all squmbles of the user
+export async function getSqumbles() {
+    // ... 
+}
+
+// Gets squmble by ID
+export async function getSqumble(id) {
+    // ...
+}
+
+// Updates the squggle by id
+export async function updateSquggle(id, data) {
+    // ...
+}
+
+// Creates a squagel
+export async function createSquagel(data) {
+    // ...
+}
